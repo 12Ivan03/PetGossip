9ff5d17cb8cb8e550fd04e3f3b31f4cf826b24c8
ttahb <bhattvijay69@hotmail.com>
2024-01-20 19:10:50 +0530

    preload images code - not tested

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,44 @@
 // Code to run when the window has fully loaded
+
+window.onload = function () {
+  function preloadImages(images, cb) {
+    let loaded = 0;
+    const numImages = images.length;
+
+    function imageLoaded() {
+      loaded++;
+      if (loaded === numImages) {
+        cb();
+      }
+    }
+
+    images.forEach(src => {
+      const img = new Image();
+      img.onload = imageLoaded;
+      img.src = src;
+    });
+  }
+
+  // List of image URLs to preload
+  const imageFolder = 'public/images/';
+  const imageUrls = [
+    'gras.jpeg',
+    'woman-walking-with-her-dog.jpeg',
+    'isolated-on-blue-background.jpeg',
+    'petgossip-logo.png',
+    'petgossip-logo-black-transparent.png',
+    'running-in-grass-dog.jpeg',
+    'petgossip-end-logo-transparent.png',
+    'white-horizontal-website-banner.jpeg'
+
+  ];
+
+  preloadImages(imageUrls.map(url => imageFolder + url), function () {
+    console.log('All images preloaded.');
+    // Continue with your website initialization or show content
+  });
+}
+
 document.addEventListener("DOMContentLoaded",() => {
       
   //// Pet profile ////
