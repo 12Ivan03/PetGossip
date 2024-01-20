ab05cfad5b000af179fc56e6ef27c929e61a8da8
ttahb <bhattvijay69@hotmail.com>
2024-01-20 13:59:32 +0530

    fixing render . com

--- config/session.config.js
+++ config/session.config.js
@@ -1,5 +1,7 @@
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/PetGossipView";
 
 module.exports = app => {
     app.set('trust proxy', 1);
@@ -12,7 +14,7 @@ module.exports = app => {
                 maxAge: 1000 * 60 * 60 * 24
             },
             store: MongoStore.create({ 
-                mongoUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/PetGossipView"
+                mongoUrl: MONGO_URI
             })
         })
     );
