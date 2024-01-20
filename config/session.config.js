88d4b4c04c2d5b6661ff34cb1daa844ae0acfa0e
ttahb <bhattvijay69@hotmail.com>
2024-01-20 14:20:00 +0530

    node environment -

--- config/session.config.js
+++ config/session.config.js
@@ -11,7 +11,7 @@ module.exports = app => {
             resave: false,
             saveUninitialized: false,
             cookie: {
-                secure:false,
+                secure:process.env.NODE_ENV === 'production',
                 maxAge: 1000 * 60 * 60 * 24
             },
             store: MongoStore.create({ 
