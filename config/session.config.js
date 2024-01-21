c0918253d1b64fdc26964989d9cc6fec581f98ef
ttahb <bhattvijay69@hotmail.com>
2024-01-21 12:21:35 +0530

    session management

--- config/session.config.js
+++ config/session.config.js
@@ -11,12 +11,15 @@ module.exports = app => {
             resave: false,
             saveUninitialized: false,
             cookie: {
-                secure:process.env.NODE_ENV === 'production',
-                maxAge: 1000 * 60 * 60 * 24
+                secure:process.env.NODE_ENV === 'prod',
+                //7 days
+                maxAge: 1000 * 60 * 60 * 24 * 7
+                // maxAge: 30000
             },
             store: MongoStore.create({ 
                 mongoUrl: MONGO_URI
-            })
+            }),
+            rolling: true // Set rolling to true to update the last access time on every request
         })
     );
 };
